e8884e81986aa215fda6a8fe80f888658022eb9a
egyr101 <164477129+egyr101@users.noreply.github.com>
2024-08-24 17:08:46 +0500

    Add files via upload

--- work-django/main/models.py
+++ work-django/main/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=256,unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
-    image = models.ImageField(upload_to = 'product_images',default='images.jfif')
+    image = models.ImageField(upload_to = 'main/static/images',default='img.png')
     category = models.ForeignKey(to=ProductCategory,on_delete=models.CASCADE)
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
