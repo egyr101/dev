f45363b8dd073865a42ef0e467cec50cf1f136e4
egyr101 <164477129+egyr101@users.noreply.github.com>
2024-09-08 13:34:15 +0500

    Add files via upload

--- work-django/main/models.py
+++ work-django/main/models.py
@@ -29,7 +29,8 @@ class Product(models.Model):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 class Page(models.Model):
     page_direction = 0
-    url = 'http://195.43.142.204:8000'
+    url = 'http://127.0.0.1:8000'
     check_quality = True
     check_man = False
     check_woman = False
+    search_name = ' '
--- work-django/main/views.py
+++ work-django/main/views.py
@@ -4,149 +4,111 @@ from django.utils.text import slugify
 Page.page_direction = 0
 Page.check_woman = False
 Page.check_man = False
+def unloading_base(a,page):
+    items = list(Product.objects.filter(category=a))
+    if len(items) == 0:
+        Page.check_quality = False
+    else:
+        items = items[page * 8:(page + 1) * 8]
+        return items
+def unloading_gender(a,gen,page):
+    items = list(Product.objects.filter(category=a,gender=gen))
+    if len(items) == 0:
+        Page.check_quality = False
+    else:
+        items = items[page*8:(page+1)*8]
+        return items
+def unloading_searchname(a,page,search):
+    items = list(Product.objects.filter(category=a, name__icontains=search))
+    if len(items) == 0:
+        Page.check_quality = False
+    else:
+        items = items[page * 8:(page + 1) * 8]
+        return items
+def unloading_searchname_gender(a,page,search,gen):
+    items = list(Product.objects.filter(category=a, name__icontains=search,gender=gen))
+    if len(items) == 0:
+        Page.check_quality = False
+    else:
+        items = items[page * 8:(page + 1) * 8]
+        return items
+
 def pag(request,a):
+    items = []
     if request.method == 'POST':
-        items = []
         if 'pre' in request.POST:
             Page.page_direction-=1
             if Page.check_man:
-                l = 0
-                for prod in Product.objects.filter(category=a,gender='man'):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                items=unloading_gender(a,'man',Page.page_direction)
             elif Page.check_woman:
-                l = 0
-                for prod in Product.objects.filter(category=a, gender='woman'):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                items=unloading_gender(a, 'woman', Page.page_direction)
             else:
-                l = 0
-                for prod in Product.objects.filter(category=a):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                items=unloading_base(a,Page.page_direction)
         elif 'next' in request.POST:
             Page.page_direction+=1
             if Page.check_man:
-                l = 0
-                for prod in Product.objects.filter(category=a, gender='man'):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                items=unloading_gender(a, 'man', Page.page_direction)
             elif Page.check_woman:
-                l = 0
-                for prod in Product.objects.filter(category=a, gender='woman'):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                items=unloading_gender(a, 'woman', Page.page_direction)
             else:
-                l = 0
-                for prod in Product.objects.filter(category=a):
-                    l += 1
-                    if l > Page.page_direction * 8 + 8:
-                        l = 0
-                        break
-                    elif l <= 8 * Page.page_direction + 8 and (l > Page.page_direction * 8):
-                        items = items + [prod]
-                    else:
-                        continue
+                unloading_base(a, Page.page_direction)
         elif 'search-name' in request.POST:
-            search_name = request.POST.get('search-str',1)
+            Page.search_name = request.POST.get('search-str',1)
             if Page.check_man:
-                for item in Product.objects.filter(name__icontains=search_name,category=a,gender='man'):
-                    items = items + [item]
-                if len(items) == 0:
-                    Page.check_quality = False
-                else:
-                    Page.check_quality = True
+                items=unloading_searchname_gender(a,Page.page_direction,Page.search_name,'man')
             elif Page.check_woman:
-                for item in Product.objects.filter(name__icontains=search_name,category=a,gender='woman'):
-                    items = items + [item]
-                if len(items) == 0:
-                    Page.check_quality = False
-                else:
-                    Page.check_quality = True
+                items=unloading_searchname_gender(a, Page.page_direction, Page.search_name, 'woman')
             else:
-                for item in Product.objects.filter(name__icontains=search_name,category=a):
-                    items = items + [item]
-                if len(items) == 0:
-                    Page.check_quality = False
-                else:
-                    Page.check_quality = True
+               items=unloading_searchname(a,Page.page_direction, Page.search_name)
         elif 'man' in request.POST:
             Page.check_man = True
             Page.check_woman = False
-            items =[]
-            l = Page.page_direction * 8
-            for prod in Product.objects.filter(category=a,gender='man'):
-                l += 1
-                if l <= 8 * Page.page_direction + 8:
-                    items = items + [prod]
-                else:
-                    break
+            Page.page_direction = 0
+            items=unloading_gender(a,'man',Page.page_direction)
         elif 'woman' in request.POST:
             Page.check_man = False
             Page.check_woman = True
-            items =[]
-            l = Page.page_direction * 8
-            for prod in Product.objects.filter(category=a,gender='woman'):
-                l += 1
-                if l <= 8 * Page.page_direction + 8:
-                    items = items + [prod]
-                else:
-                    break
+            Page.page_direction = 0
+            items=unloading_gender(a,'woman',Page.page_direction)
 
     else:
-        items = []
-        l = Page.page_direction*8
-        for prod in Product.objects.filter(category=a):
-            l += 1
-            if l <= 8 * Page.page_direction + 8:
-                items = items + [prod]
+        items=unloading_base(a,Page.page_direction)
+    if Page.check_quality == True:
+        if Page.check_man:
+            if Page.search_name != ' ':
+                if items[-1].id == Product.objects.filter(name__icontains=Page.search_name, category=a,gender='man').last().id:
+                    check_last = False
+                else:
+                    check_last = True
             else:
-                break
-    if Page.check_man:
-        if items[-1].id == Product.objects.filter(category=a,gender='man').last().id:
-            check_last = False
+                if items[-1].id == Product.objects.filter(category=a, gender='man').last().id:
+                    check_last = False
+                else:
+                    check_last = True
+        elif Page.check_woman:
+            if Page.search_name != ' ':
+                if items[-1].id == Product.objects.filter(name__icontains=Page.search_name,category=a,gender='woman').last().id:
+                    check_last = False
+                else:
+                    check_last = True
+            else:
+                if items[-1].id == Product.objects.filter(category=a,gender='woman').last().id:
+                    check_last = False
+                else:
+                    check_last = True
         else:
-            check_last = True
-    elif Page.check_woman:
-        if items[-1].id == Product.objects.filter(category=a,gender='woman').last().id:
-            check_last = False
-        else:
-            check_last = True
+            if Page.search_name !=' ':
+                if items[-1].id == Product.objects.filter(category=a,name__icontains=Page.search_name).last().id:
+                    check_last = False
+                else:
+                    check_last = True
+            else:
+                if items[-1].id == Product.objects.filter(category=a).last().id:
+                    check_last = False
+                else:
+                    check_last = True
     else:
-        if items[-1].name == Product.objects.filter(category=a).last().name:
-            check_last = False
-        else:
-            check_last = True
+        check_last = False
     context ={
         'products': items,
         'pre': Page.page_direction,
@@ -156,6 +118,10 @@ def pag(request,a):
     return context
 def index(request):
     Page.page_direction = 0
+    Page.check_woman = False
+    Page.check_man = False
+    Page.search_name = ' '
+    Page.check_quality = True
     context = {
         'url':Page.url
     }
@@ -178,6 +144,7 @@ def accessories(request):
 def sunglasses(request):
     return render(request, 'main/prodcatalog.html',pag(request,6))
 def product(request,category,id):
+    Page.page_direction = 0
     context = {
         'product':Product.objects.get(id=id),
 
